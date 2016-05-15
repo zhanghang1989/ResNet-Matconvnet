c746e513aa5360e44baa475e5c64c319007d163a
Hang Zhang <Hang Zhang>
2016-05-15 11:04:22 -0400

    imagenet

--- res_imagenet.m
+++ res_imagenet.m
@@ -149,7 +149,7 @@ if nargout > 0
     im = gpuArray(im) ;
   end
   labels = imdb.images.label(batch) ;
-  inputs = {'image', im, 'label', labels} ;
+  inputs = {'data', im, 'label', labels} ;
 end
 
 % -------------------------------------------------------------------------
